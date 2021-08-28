67e62bf98d9605ddf0df069d4eb3a735e5ae5515
chiefexb <chief_exb@mail.ru>
2021-08-28 14:48:42 +0300

    1

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context, Template
+
 
 # Create your views here.
 def index (request):
 
-    html="HHH"
+
     now = datetime.datetime.now()
+    t = get_template('index.html')
+    html = t.render(context={}, request=None)
     return HttpResponse(html)
+
+# def index (request):
+#     p = Vitrina.objects.all()
+#     #a = p.values()
+#
+#     t = get_template('front/build/index.html')
+#     html = t.render(context={'bd':bd,'items':p,'date_now':str(datetime.datetime.now() )}, request=None)
+#     return HttpResponse(html)
